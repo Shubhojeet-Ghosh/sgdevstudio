c30c9e98bd3685352310ffcba4c4c31d90b028c3
Shubhojeet Ghosh <shubhojeet.official@gmail.com>
2025-03-12 18:48:30 +0530

    Add TopNavigation component and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import React from "react";
+import TopNavigation from "@/components/TopNavigation/TopNavigation";
 export default function () {
   return (
     <>
-      <div className="text-[30px]">Hello, Shubhojeet Ghosh this side...</div>
+      <TopNavigation />
+      <div className="w-full px-[180px] text-[72px] font-[600] text-[#334155]">
+        I'm Shubhojeet Ghosh,
+      </div>
     </>
   );
 }
---
+++ src/components/TopNavigation/TopNavigation.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TopNavigation = () => {
+  return <div className="w-full h-[50px] bg-red-100"></div>;
+};
+
+export default TopNavigation;
