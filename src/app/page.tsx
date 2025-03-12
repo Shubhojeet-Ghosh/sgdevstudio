2461734f05373cf80be6d8a03694ca92b38bc862
Shubhojeet Ghosh <shubhojeet.official@gmail.com>
2025-03-13 02:05:55 +0530

    font style and color schemas testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,20 @@ import TopNavigation from "@/components/TopNavigation/TopNavigation";
 export default function () {
   return (
     <>
-      <TopNavigation />
-      <div className="w-full px-[180px] text-[72px] font-[600] text-[#334155]">
-        I'm Shubhojeet Ghosh,
+      <div className="bg-black min-h-[100dvh]">
+        <TopNavigation />
+        <div className="w-full px-[180px] text-[72px] font-[800] text-[#EEEEEE]">
+          I'm <span className="text-[#00ADB5]">S</span>hubhojeet{" "}
+          <span className="text-[#00ADB5]">G</span>hosh,
+        </div>
+        <div className="w-full px-[180px]">
+          <div className="w-[28%]">
+            <p className="text-[20px] text-[#EEEEEE] w-full font-[400]">
+              A <span className="font-[800]">full-stack developer</span> who
+              loves intuitive, clean and modern UI design.
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/TopNavigation/TopNavigation.tsx
+++ src/components/TopNavigation/TopNavigation.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const TopNavigation = () => {
-  return <div className="w-full h-[50px] bg-red-100"></div>;
+  return (
+    <div className="sticky top-0 left-0 w-full h-[60px] bg-black/80 backdrop-blur-[10px] shadow-md z-[100] text-[#EEEEEE] flex items-center px-6">
+      <div className="text-[20px] font-[700]">
+        <span className="text-[#00ADB5]">sg</span>devstudio
+      </div>
+    </div>
+  );
 };
 
 export default TopNavigation;
