206231090c7baaccf9f8ea11c7769809d7855a8e
Shubhojeet Ghosh <shubhojeet.official@gmail.com>
2025-03-13 14:53:27 +0530

    metadata and favicon image set

---
+++ src/components/BrandLogo/SgDevStudioLogo.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SgLogo = () => {
+  return (
+    <>
+      <div className="w-full">
+        <div className="font-[700]">
+          <span className="text-[#00ADB5]">sg</span>devstudio
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SgLogo;
--- src/components/TopNavigation/TopNavigation.tsx
+++ src/components/TopNavigation/TopNavigation.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import SgLogo from "@/components/BrandLogo/SgDevStudioLogo";
 const TopNavigation = () => {
   return (
     <div className="sticky top-0 left-0 w-full h-[60px] bg-black/80 backdrop-blur-[10px] shadow-md z-[100] text-[#EEEEEE] flex items-center px-6">
-      <div className="text-[20px] font-[700]">
-        <span className="text-[#00ADB5]">sg</span>devstudio
+      <div className="text-[20px]">
+        <SgLogo />
       </div>
     </div>
   );
