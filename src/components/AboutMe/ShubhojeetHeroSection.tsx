d700e11655dac2c83327de5941ead4e9a952eee6
Shubhojeet Ghosh <shubhojeet.official@gmail.com>
2025-12-21 22:45:34 +0530

    Refactor ShubhojeetHeroSection: Change greeting from h1 to p tag for semantic clarity and adjust heading structure for improved accessibility.

--- src/components/AboutMe/ShubhojeetHeroSection.tsx
+++ src/components/AboutMe/ShubhojeetHeroSection.tsx
@@ -5,16 +5,16 @@ export default function ShubhojeetHeroSection() {
   return (
     <div className="flex flex-col justify-center dark:text-brightgray text-darkerblack">
       <div className="w-full animate-blur-reveal">
-        <h1 className="text-[20px] md:text-[24px] font-[800] leading-[0.9] text-pastelprimarygreen">
+        <p className="text-[20px] md:text-[24px] font-[800] leading-[0.9] text-pastelprimarygreen">
           Hi,
-        </h1>
+        </p>
       </div>
-      <div className="w-full text-[32px] md:text-[72px] font-[700] animate-blur-reveal leading-[0.9] mt-[20px]">
+      <h1 className="w-full text-[32px] md:text-[72px] font-[700] animate-blur-reveal leading-[0.9] mt-[20px]">
         I&apos;m{" "}
         <span className="bg-gradient-to-r from-gradientpurple to-gradientpink bg-clip-text text-transparent">
           Shubhojeet
         </span>
-      </div>
+      </h1>
       <div className="w-full mt-[16px] text-[16px] md:text-[24px]">
         <TextAnimate animation="blurIn">
           Software engineer specializing in AI systems, agents, and web
